fe6a0d2ffa3f47201e5b4ca1304979b984ab40ae
Taroka <kiy535353@hotmail.com>
2014-06-18 02:15:14 +0800

    Uno commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '5933d0ba47c7b135aaed581ca6c8ab992a5fa8e7b4379dcffb97d34f8b6c9e78281ac8c59a349ae8e3ef97393d0daaa7249f1f9f8069e1ba2dfb4ddb2c8aad7e'
